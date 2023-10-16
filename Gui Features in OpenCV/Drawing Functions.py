7f8243d906aa3fd8a51f47b5aa112da3392745ba
Mohammed Eid Abdelmeguid Ibrahim <mohammedeidabdelmeguid@gmail.com>
2023-10-16 20:10:36 +0300

    Update Drawing functions eith exercise

--- Gui Features in OpenCV/Drawing Functions.py
+++ Gui Features in OpenCV/Drawing Functions.py
@@ -26,3 +26,34 @@ cv.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv.LINE_AA)
 cv.imshow('Coded image',img)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+
+# Create a white canvas
+img = np.ones((512, 512, 3), np.uint8) * 255
+
+# Define the colors of the logo
+red = (0, 0, 255)
+green = (0, 255, 0)
+blue = (255, 0, 0)
+white = (255, 255, 255)
+
+# Draw the three circles
+cv.circle(img, (127, 44), 70, red, -1)
+cv.circle(img, (74, 134), 70, green, -1)
+cv.circle(img, (179, 134), 70, blue, -1)
+
+# Draw the white outlines
+cv.circle(img, (127, 44), 30, white, -1)
+cv.circle(img, (74, 134), 30, white, -1)
+cv.circle(img, (179, 134), 30, white, -1)
+cv.ellipse(img, (127, 44), (70, 70), 60, 0, 60, white, -1)
+cv.ellipse(img, (74, 134), (70, 70), 360 - 60, 0, 60, white, -1)
+cv.ellipse(img, (179, 134), (70, 70), 360 - 2 * 60, 60, 0, white, -1)
+
+# Write the text "OpenCV"
+cv.putText(img, 'OpenCV', (32, 240), cv.FONT_HERSHEY_SIMPLEX, 1.65, (0, 0, 0), 5, cv.LINE_AA)
+
+# Display the OpenCV logo
+cv.imshow('OpenCV Logo', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
