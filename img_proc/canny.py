3f55996bfc4b31b6411c6e61c9b25069f4eec269
Khaled Basiony <basiony.khaled@outlook.com>
2023-10-28 08:07:44 +0200

    use trackbars for canny parameter tuning

--- img_proc/canny.py
+++ img_proc/canny.py
@@ -2,10 +2,23 @@ import cv2
 import numpy as np
 
 img = cv2.imread('../assets/sudoku.jpg', 0)
-edges = cv2.Canny(img, 50, 100, L2gradient=True)
+cv2.imshow('original', img)
+cv2.namedWindow('playground')
 
-cv2.imshow('Original', img)
-cv2.imshow('Edges', edges)
+cv2.createTrackbar('thresh1', 'playground', 0, 255, lambda x: ...)
+cv2.createTrackbar('thresh2', 'playground', 0, 255, lambda x: ...)
+cv2.createTrackbar('L2', 'playground', 0, 1, lambda x: ...)
 
-cv2.waitKey(0)
+while (1):
+    edges = cv2.Canny(
+        img,
+        cv2.getTrackbarPos('thresh1', 'playground'),
+        cv2.getTrackbarPos('thresh2', 'playground'),
+        L2gradient=cv2.getTrackbarPos('L2', 'playground') == 1
+    )
+    cv2.imshow('playground', edges)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == ord('q'):
+        break
 cv2.destroyAllWindows()
