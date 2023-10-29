90a438365d8f2c11f821a97c2f0c48e8e0362256
tahany682 <84343970+tahany682@users.noreply.github.com>
2023-10-29 21:46:51 +0200

    Examples of the image processing chapter

---
+++ image processing/img trans2.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+img = cv2.imread('sudokusmall.png')
+rows,cols,ch = img.shape
+pts1 = np.float32([[56,65],[568,52],[48,587],[589,590]])
+pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+
+M = cv2.getPerspectiveTransform(pts1,pts2)
+dst = cv2.warpPerspective(img,M,(300,300))
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
+plt.show()
